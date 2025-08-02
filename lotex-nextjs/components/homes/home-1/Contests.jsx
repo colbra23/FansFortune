704d7851b13d53b564bc788b8bdba2060b083342
colbra23 <colinteacher23@gmail.com>
2025-08-03 01:30:21 +0930

    Add Modal Trigger to 90 Bingo Span

--- lotex-nextjs/components/homes/home-1/Contests.jsx
+++ lotex-nextjs/components/homes/home-1/Contests.jsx
@@ -111,17 +111,19 @@ export default function Contests() {
                       <div className="content">
                         <div className="heading">
                           <h4 className="title fw-9">
-                            <span
-                              onClick={(e) => {
-                                e.preventDefault();
-                                if (typeof window !== 'undefined' && window.openPrizeModal) {
-                                  window.openPrizeModal(card.id);
-                                }
-                              }}
-                              style={{ cursor: 'pointer', color: 'var(--White)' }}
-                            >
-                              {card.title}
-                            </span>
+                            <Link href={`/contest-details/${card.id}`}>
+                              <span
+                                onClick={(e) => {
+                                  e.preventDefault();
+                                  if (typeof window !== 'undefined' && window.openPrizeModal) {
+                                    window.openPrizeModal(card.id);
+                                  }
+                                }}
+                                style={{ cursor: 'pointer', color: 'var(--White)' }}
+                              >
+                                {card.title}
+                              </span>
+                            </Link>
                           </h4>
                           <ul className="sub-title-list">
                             <li className="item">
