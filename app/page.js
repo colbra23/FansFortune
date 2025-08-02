994c64b8be6e2dbef8daed633798a9689e9dacc6
colbra23 <colinteacher23@gmail.com>
2025-08-02 21:51:06 +0930

    Restore original home page content

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
 import Footer1 from "@/components/footers/Footer1";
 import Header1 from "@/components/headers/Header1";
+import Banner from "@/components/homes/home-1/Banner";
+import Banner2 from "@/components/homes/home-1/Banner2";
+import Banner3 from "@/components/homes/home-1/Banner3";
+import Contests from "@/components/homes/home-1/Contests";
+import CustomerSupport from "@/components/common/CustomerSupport";
+import Facts from "@/components/common/Facts";
+import Faqs from "@/components/common/Faqs";
+import Hero from "@/components/homes/home-1/Hero";
+import Steps from "@/components/homes/home-1/Steps";
+import Testimonials from "@/components/homes/home-1/Testimonials";
+import TopWinners from "@/components/homes/home-1/TopWinners";
+import Winners from "@/components/common/Winners";
 import React from "react";
 
 export const metadata = {
@@ -11,11 +23,19 @@ export default function Home() {
   return (
     <>
       <Header1 />
+      <Hero />
       <div className="main-content">
-        <div style={{ padding: '100px 0', textAlign: 'center' }}>
-          <h1>Welcome to Lotex</h1>
-          <p>Your lottery platform is loading...</p>
-        </div>
+        <Banner />
+        <Steps />
+        <Contests />
+        <Winners />
+        <Banner2 />
+        <TopWinners />
+        <Facts />
+        <Testimonials />
+        <Banner3 />
+        <Faqs />
+        <CustomerSupport />
       </div>
       <Footer1 />
     </>
