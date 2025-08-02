f43d689c264e357c8ffabed79470b47226f2e802
colbra23 <colinteacher23@gmail.com>
2025-08-02 20:09:46 +0930

    Deleted jsconfig.json

--- lotex-reactjs/lotex-reactjs/src/data/brands.js
+++
@@ -1,8 +0,0 @@
-export const brandImages = [
-  "/images/brand/brand-1.png",
-  "/images/brand/brand-2.png",
-  "/images/brand/brand-3.png",
-  "/images/brand/brand-4.png",
-  "/images/brand/brand-5.png",
-  "/images/brand/brand-6.png",
-];
--- lotex-reactjs/lotex-reactjs/src/utils/toggleMobileMenu.js
+++
@@ -1,6 +0,0 @@
-export const openMobileMenu = () => {
-  document.querySelector(".mobile-nav-wrap")?.classList.add("active");
-};
-export const closeMobileMenu = () => {
-  document.querySelector(".mobile-nav-wrap")?.classList.remove("active");
-};
